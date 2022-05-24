4bb6bc231a3401eb6c22d7eac853546710177d9a
Cas Steigstra <cas.steigstra@gmail.com>
2022-05-25 01:39:03 +0200

    Added gradient clipping

--- train_cls.py
+++ train_cls.py
@@ -47,6 +47,7 @@ def parse_args():
     parser.add_argument('--data_path', type=str, default='data/modelnet40_normal_resampled/', help='Data path')
     parser.add_argument('--activ', type=str, default=None, help='Activation function [default: author LeakyReLU]',
                         choices=ACTIV_MAP.keys())
+    parser.add_argument('--grad_clip', type=float, default=1, help='Gradient clipping [default: 1]')
     return parser.parse_args()
 
 def test(model, loader, num_class=40): #FIXME: num_class is never defined
@@ -209,6 +210,7 @@ def main(args):
             correct = pred_choice.eq(target.long().data).cpu().sum()
             mean_correct.append(correct.item() / float(points.size()[0]))
             loss.backward()
+            torch.nn.clip_grad_norm_(classifier.parameters(), args.grad_clip)
             optimizer.step()
             global_step += 1
 
